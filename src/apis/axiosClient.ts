e41e33ce581acdd0f36636a1e777e3b3ef6022fc
PhiLong28 <longpphingo123@gmail.com>
2024-04-01 10:21:27 +0700

    update create api

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -1,33 +1,34 @@
-import axios from "axios";
-import queryString from "query-string";
+import axios from 'axios';
+import queryString from 'query-string';
+import {appInfos} from '../constants/appInfos';
 
 const axiosClient = axios.create({
-    paramsSerializer: params => queryString.stringify(params)
-
-})
+  baseURL: appInfos.BASE_URL,
+  paramsSerializer: params => queryString.stringify(params),
+});
 
 axiosClient.interceptors.request.use(async (config: any) => {
-    config.headers = {
-        Authorization: '',
-        Accept:'application/json',
-        ...config.headers
+  config.headers = {
+    Authorization: '',
+    Accept: 'application/json',
+    ...config.headers,
+  };
 
-    }
-    config.data
+  config.data;
+  return config;
+});
 
-    return config
-})
-
-axiosClient.interceptors.response.use(res => {
+axiosClient.interceptors.response.use(
+  res => {
     if (res.data && res.status === 200) {
-        return res.data
+      return res.data;
     }
-     throw new Error('Error');
-}, error => {
+    throw new Error('Error');
+  },
+  error => {
     console.log(`Error api ${JSON.stringify(error)}`);
     throw new Error(error.response);
-})
+  },
+);
 
-export default axiosClient
-
-   
+export default axiosClient;
